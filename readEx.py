a81d3912de8cad27dcc14214f6cb1ab128f32daa
Andrew Chumich <achumich@achumich-AO722.(none)>
2013-02-06 13:30:21 -0700

    fixed the regex

--- readEx.py
+++ readEx.py
@@ -1,5 +1,12 @@
 import re
-p= re.compile(r'[<>]')
+
 f = open('log.txt', "r")
-print p.split(f.read())
+j = f.read()
+d = re.findall(r'(?<=\<)(.*?)(?=\>)', j)
+m = re.findall(r'(?<=\{)(.*?)(?=\})', j)
+fe = re.findall(r'(?<=\[)(.*?)(?=\])', j)
+
+
+print d[1], m[1], fe[1]
+print "\n", int(m[0])+int(m[1])
 f.close()
