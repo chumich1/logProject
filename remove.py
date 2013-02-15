c23616b08d9743d131b8731981f15767392fdb07
Andrew Chumich <achumich@achumich-AO722.(none)>
2013-02-14 18:51:04 -0700

    Gave option to not remove mid-execution

--- remove.py
+++ remove.py
@@ -7,7 +7,7 @@ m = re.findall(r'(?<=\[)(.*?)(?=\])', j)
 t = re.findall(r'(?<=\:_;)(.*?)(?=\;_:)', j)
 fe = re.findall(r'(?<=\{)(.*?)(?=\})', j)
 f.close()
-print "Which would you like to remove? (enter the integer on the left side)"
+print "Which would you like to remove? (enter 0 for none)"
 
 i = 0
 while i<len(d): 
@@ -15,21 +15,23 @@ while i<len(d):
     i =i+1
 
 x = input("Workout #: ")
-del d[x-1]
-del m[x-1]
-del t[x-1]
-del fe[x-1]
+if x != 0:
+
+    del d[x-1]
+    del m[x-1]
+    del t[x-1]
+    del fe[x-1]
 
 
 
 
-f = open('log.txt','w')
+    f = open('log.txt','w')
 
-i = 0
-while i<len(d):
-    f.write("<"+d[i]+">"+"["+m[i]+"]"+"{"+fe[i]+"}"+":_;"+t[i]+";_:") 
-    i =i+1
+    i = 0
+    while i<len(d):
+        f.write("<"+d[i]+">"+"["+m[i]+"]"+"{"+fe[i]+"}"+":_;"+t[i]+";_:") 
+        i =i+1
 
-f.close()
+    f.close()
 
 
